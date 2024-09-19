412593a8df39fb12dcb8ede758e17155ca7dc7f2
AustineDavid <davmesoaustine@gmail.com>
2024-09-19 10:00:37 +0100

    show addeds

--- components/Meeting/MeetingProper.tsx
+++ components/Meeting/MeetingProper.tsx
@@ -1,19 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CallControls,
   CallParticipantsList,
   PaginatedGridLayout,
+  useCallStateHooks,
+  SpeakerLayout,
 } from "@stream-io/video-react-sdk";
 import { MdGroups2 } from "react-icons/md";
 import { useRouter } from "next/navigation";
 
+const LayoutMap = {
+  Speaker: {
+    component: SpeakerLayout,
+    title: "Speaker layout with spotlight",
+    props: {},
+  },
+  PaginatedGrid: {
+    component: PaginatedGridLayout,
+    title: "Paginated grid layout",
+    props: {
+      groupSize: 12,
+    },
+  },
+};
+
 const MeetingProper = () => {
+  const [selectedLayout, setSelectedLayout] =
+    useState<keyof typeof LayoutMap>("PaginatedGrid");
   const [showUser, setShowUser] = useState<boolean>(false);
+  const { useHasOngoingScreenShare } = useCallStateHooks();
+  const hasOngoingScreenshare = useHasOngoingScreenShare();
   const router = useRouter();
+
+  const LayoutComponent = LayoutMap[selectedLayout].component;
+  const componentProps = LayoutMap[selectedLayout].props;
+
+  useEffect(() => {
+    // set screen share compatible layout
+    if (hasOngoingScreenshare) return setSelectedLayout("Speaker");
+
+    return setSelectedLayout("PaginatedGrid");
+  }, [hasOngoingScreenshare]);
   return (
     <section className=" relative w-full h-screen text-white">
       <div className=" relative w-full h-screen text-white overflow-hidden pt-4">
-        <PaginatedGridLayout />
+        <LayoutComponent {...componentProps} />
       </div>
       <div className=" fixed bottom-0 px-2  gap-4  z-50 w-full flex items-center justify-center ">
         <CallControls
